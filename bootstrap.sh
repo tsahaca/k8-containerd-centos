2e279dcd2452a90f565c3d09992fa2503bebaa10
tsahaca <tanmay.x.saha@gmail.com>
2022-06-01 15:12:07 -0700

    working

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,57 +14,75 @@ yum update -y
 echo "[TASK# 2] Installing wget"
 yum install wget -y
 
+echo "[TASK 3] Update /etc/hosts file"
+cat >>/etc/hosts<<EOF
+172.16.16.100   kmaster.example.com    kmaster
+172.16.16.101   kworker1.example.com   kworker1
+172.16.16.102   kworker2.example.com   kworker2
+EOF
+
  
 ## Disable firewall starting from Kubernetes v1.19 onwards to avoid any issues. Kubernetes uses IPTables to handle inbound and outbound traffic.
-echo "[TASK# 1] Disable firewall"
+echo "[TASK# 4] Disable firewall"
 systemctl disable firewalld
 systemctl stop firewalld
 
 
 ## Swap needs to be disabled. Kubeadm will check to make sure that swap is disabled. Turn swap off and disable for future reboots.
-echo "[TASK# 2] Disable Swap"
+echo "[TASK# 5] Disable Swap"
 swapoff -a
 sed -i.bak '/swap/d' /etc/fstab
 
 ## We need to disable SELinux or set it to permissive mode if it’s enabled. I’m setting to Permissive mode.
-echo "[TASK# 3] Disable SELinux"
+echo "[TASK# 6] Disable SELinux"
 setenforce 0
 sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
  
 ## Setup necessary steps to use containerd as CRI runtime
-echo "[TASK# 4] Install and configure prerequisites for containerd runtime"
-cat <<EOF | tee /etc/modules-load.d/containerd.conf
+# Configure persistent loading of modules
+echo "[TASK# 7] Setup necessary steps to use containerd as CRI runtime"
+tee /etc/modules-load.d/containerd.conf <<EOF
 overlay
 br_netfilter
 EOF
 
+# Load at runtime
 modprobe overlay
 modprobe br_netfilter
 
-# Setup required sysctl params, these persist across reboots.
-cat <<EOF | tee /etc/sysctl.d/99-kubernetes-cri.conf
-net.bridge.bridge-nf-call-iptables  = 1
-net.ipv4.ip_forward                 = 1
+# Ensure sysctl params are set
+tee /etc/sysctl.d/kubernetes.conf<<EOF
 net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
 EOF
 
-# Apply sysctl params without reboot
-sysctl --system  
- 
-## Install Containerd with Release Tarball
-echo "[TASK# 5] Install Containerd with Release Tarball"
-yum install wget -y
-wget https://storage.googleapis.com/cri-containerd-release/cri-containerd-1.3.4.linux-amd64.tar.gz
-tar --no-overwrite-dir -C / -xzf cri-containerd-1.3.4.linux-amd64.tar.gz
+# Reload configs
+sysctl --system
+
+# Install required packages
+yum install -y yum-utils device-mapper-persistent-data lvm2
+
+# Add Docker repo
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+# Install containerd
+echo "[TASK# 8] Installing containerd"
+yum update -y && yum install -y containerd.io
+
+# Configure containerd and start service
 mkdir -p /etc/containerd
-containerd config default | tee /etc/containerd/config.toml
+containerd config default > /etc/containerd/config.toml
+
+# restart containerd
+echo "[TASK# 9] Restarting and Enabling containerd"
+systemctl restart containerd
 systemctl enable containerd
-systemctl start containerd
 
  
 ###
 ## configuring Kubernetes repositories
-echo "[TASK# 6] Configuring Kubernetes repositories"
+echo "[TASK# 10] Configuring Kubernetes repositories"
 cat >>/etc/yum.repos.d/kubernetes.repo<<EOF
 [kubernetes]
 name=Kubernetes
@@ -80,11 +98,11 @@ EOF
 ##Install Kubernetes, specify Version as CRI-O
 yum install -y kubelet-1.22.4-0 kubeadm-1.22.4-0 kubectl-1.22.4-0 >/dev/null 2>&1
 
-echo "[TASK# 14] Pull required containers"
+echo "[TASK# 11] Pull required containers"
 kubeadm config images pull >/dev/null 2>&1
 
 # Start and Enable kubelet service
-echo "[TASK 10] Enable and start kubelet service"
+echo "[TASK 12] Enable and start kubelet service"
 systemctl daemon-reload
 systemctl enable kubelet 
 #--now
@@ -110,11 +128,3 @@ echo "kubeadmin" | passwd --stdin root >/dev/null 2>&1
 
 # Update vagrant user's bashrc file
 echo "export TERM=xterm" >> /etc/bashrc
-
-
-echo "[TASK 13] Update /etc/hosts file"
-cat >>/etc/hosts<<EOF
-172.16.16.100   kmaster.example.com    kmaster
-172.16.16.101   kworker1.example.com   kworker1
-172.16.16.102   kworker2.example.com   kworker2
-EOF
