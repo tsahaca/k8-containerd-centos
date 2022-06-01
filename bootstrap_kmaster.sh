85a44ffb746537433e176f26efda157867705195
tsahaca <tanmay.x.saha@gmail.com>
2022-06-01 15:08:34 -0700

    working copy

--- bootstrap_kmaster.sh
+++ bootstrap_kmaster.sh
@@ -6,7 +6,7 @@
 
 # Initialize Kubernetes
 echo "[TASK# 15] Initialize Kubernetes Cluster"
-kubeadm init --apiserver-advertise-address=172.16.16.100 --pod-network-cidr=192.168.0.0/16 >> /root/kubeinit.log 2>/dev/null
+kubeadm init --apiserver-advertise-address=172.16.16.100 --pod-network-cidr=10.244.0.0/16 >> /root/kubeinit.log 2>/dev/null
 
 # Copy Kube admin config
 echo "[TASK# 16] Copy kube admin config to Vagrant user .kube directory"
@@ -17,7 +17,8 @@ chown -R vagrant:vagrant /home/vagrant/.kube
 # Deploy calico network
 echo "[TASK# 17] Deploy Calico network"
 ## su - vagrant -c "kubectl create -f https://docs.projectcalico.org/v3.18/manifests/calico.yaml"
-kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.18/manifests/calico.yaml >/dev/null 2>&1
+#kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/manifests/calico.yaml >/dev/null 2>&1
+kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f kube-flannel.yml >/dev/null 2>&1
 
 
 # Generate Cluster join command
